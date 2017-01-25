1425d23006f261dee5dbc82cd2c3f7576d24cf08
Robin <robinboitemail@gmail.com>
2017-01-25 19:59:48 +0100

    Player throw

--- Gold&Retriever/Assets/Scripte/Player.cs
+++ Gold&Retriever/Assets/Scripte/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour {
 
 	public Rigidbody2D rb; 
 	public GameObject feet ;
+    public CapsuleCollider2D capsuleCollider;
 
 	private bool isJumping = false;
 	public LayerMask isJumpable ; 
@@ -18,13 +19,17 @@ public class Player : MonoBehaviour {
 	public Animator animator ;
     public GameObject bomb;
     public float bombThrow;
-	// Touche clavier :
+    private bool holdSomething=false;
+    private GameObject heldObject;
 
-	public KeyCode toucheDroite; 
+    // Touche clavier :
+
+    public KeyCode toucheDroite; 
 	public KeyCode toucheGauche; 
 	public KeyCode toucheSaut;
     public KeyCode toucheBomb;
     public KeyCode toucheAccroupi;
+    public KeyCode toucheAction;
 
     //private Manager manager ; 
     // Use this for initialization
@@ -94,11 +99,24 @@ public class Player : MonoBehaviour {
                 b.GetComponent<Rigidbody2D>().velocity = new Vector2(bombThrow * this.transform.localScale.x, bombThrow);
             }
         }
+
+        //Action
+
+        if (Input.GetKeyDown(toucheAction))
+        {
+            doAction();
+        }
         // PIED SAUT 
 		if (Input.GetKey (toucheSaut)
 			&& (Physics2D.OverlapBox(new Vector2 (feet.transform.position.x, feet.transform.position.y), new Vector2 (0.728853f, 0.1633179f),0,isJumpable)))
 			rb.velocity = new Vector2 (rb.velocity.x, jumpPower);
 
+
+        // DEPLACE OBJET TENU
+        if (holdSomething)
+        {
+            moveHeldObject();
+        }
 		if (Physics2D.OverlapBox(new Vector2 (feet.transform.position.x, feet.transform.position.y), new Vector2 (0.728853f, 0.1633179f),0,isJumpable))
 			animator.SetBool ("Jump", false);
 		else 
@@ -145,5 +163,47 @@ public class Player : MonoBehaviour {
 	{
 		SceneManager.LoadScene (SceneManager.GetActiveScene ().name); 
 	}
+
+    void moveHeldObject()
+    {
+        Transform t = heldObject.transform;
+        t.position = new Vector3(this.transform.position.x + 0.2f * this.transform.localScale.x,
+            this.transform.position.y + 0.2f, this.transform.position.z);
+        t.localScale = this.transform.localScale;
+    }
+
+    void doAction()
+    {
+        Collider2D col = null;
+        Collider2D[] cols = Physics2D.OverlapCapsuleAll(capsuleCollider.transform.position, capsuleCollider.size, capsuleCollider.direction, 0);
+
+        if (cols.Length>1)
+        {
+            foreach (Collider2D c in cols)
+            {
+                if (c.gameObject != this.gameObject)
+                {
+                    col = c;
+                }
+            }
+            if (holdSomething)
+            {
+                //col.gameObject.GetComponent<Player>(); activate
+                heldObject.GetComponent<Rigidbody2D>().velocity = new Vector2(20 * this.transform.localScale.x, 10);
+                holdSomething = false;
+                //heldObject = null;
+            }
+            else
+            {
+                if (col.gameObject.tag == "Player")
+                {
+                    heldObject = col.gameObject;
+                    //col.gameObject.GetComponent<Player>(); inactivate
+                    holdSomething = true;
+                }
+            }
+        }
+       
+    }
 }
 
