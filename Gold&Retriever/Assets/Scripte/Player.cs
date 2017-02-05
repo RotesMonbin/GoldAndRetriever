06b95714264500731ed48db2987793cbe769b826
RotesMobin <robinboitemail@gmail.com>
2017-02-05 16:27:44 +0100

    rope climb ++

--- Gold&Retriever/Assets/Scripte/Player.cs
+++ Gold&Retriever/Assets/Scripte/Player.cs
@@ -639,7 +639,7 @@ public class Player : MonoBehaviour {
 		if (onLadder) {
 			if (Input.GetKey (toucheHaut) || manetteUp())
 				rb.velocity = new Vector2 (0, acceleration * climbSpeed); 
-			else if (Input.GetKey (toucheAccroupi))
+			else if (Input.GetKey (toucheAccroupi) || manetteDown())
 				rb.velocity = new Vector2 (0, -acceleration * climbSpeed); 
 		
 			rb.gravityScale = 0;
@@ -680,7 +680,7 @@ public class Player : MonoBehaviour {
 
     bool manetteDown()
     {
-
+        Debug.Log(Input.GetAxis(manetteAxeY));
         return Input.GetAxis(manetteAxeY ) > 0.5;
     }
     bool manetteUp()
