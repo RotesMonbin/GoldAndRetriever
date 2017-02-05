ad933e724faa802ef1555b353f6763bd446420be
Nikosaul <Nicolas.saul@etu.univ-lyon1.fr>
2017-02-05 18:11:31 +0100

    Chunk

--- Gold&Retriever/Assets/Scripte/chestSpawn.cs
+++ Gold&Retriever/Assets/Scripte/chestSpawn.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class chestSpawn : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class chestSpawn : MonoBehaviour
+{
+    public List<GameObject> objlist;
+    int randomNbTaper; 
+    // Use this for initialization
+    void Start()
+    {
+        randomNbTaper = Random.Range(1,3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            if(randomNbTaper != 0)
+            {
+                int r2 = Random.Range(5, 10);
+                for (int i = 0; i < r2; i++)
+                {
+                    GameObject gameobjTemp = Instantiate(objlist[0]);
+                   
+                    int r = Random.Range(-3, 3);
+                    gameobjTemp.GetComponent<Rigidbody2D>().velocity = new Vector2(r, 5.0f);
+                    Vector3 temp = this.transform.position;
+                    temp.y = temp.y + 1;
+                    gameobjTemp.transform.position = temp;
+                }
+                randomNbTaper--;
+            }else
+            {
+                Destroy(this.gameObject);
+            }
+          
+          
+
+        }
+
+    }
+
 }
