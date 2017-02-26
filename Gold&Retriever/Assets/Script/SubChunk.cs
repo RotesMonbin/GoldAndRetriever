49d4d8c77f2de20300d3ddbbc8c8d2c103a33236
RotesMobin <robinboitemail@gmail.com>
2017-02-26 17:42:14 +0100

    First SubChunk

---
+++ Gold&Retriever/Assets/Script/SubChunk.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubChunk : MonoBehaviour {
+
+    public List<GameObject> SubChunks;
+
+    // Use this for initialization
+    void Awake () {
+        int ind = Random.Range(0, SubChunks.Count);
+        GameObject instance= Instantiate(SubChunks[ind],this.transform.parent.transform);
+        instance.transform.position = this.gameObject.transform.position;
+        Destroy(this.gameObject);
+	}
+}
