1bdd00503eadf8e6d3de8cd8257e70ac632e4b4a
NikoSaul <niikosaul@gmail.com>
2017-02-28 14:04:12 +0100

    spider OK ++

--- Gold&Retriever/Assets/Script/Monstre/SpiderSimple.cs
+++ Gold&Retriever/Assets/Script/Monstre/SpiderSimple.cs
@@ -20,6 +20,8 @@ public class SpiderSimple : MonoBehaviour
 
     private float timeDelta = 0;
 
+    [SerializeField]
+    private GameObject web; 
     // Use this for initialization
     void Start()
     {
@@ -32,7 +34,6 @@ public class SpiderSimple : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
         float dt = 0.1f * speed;
         float posYMax = posInit.y - distance;
         float posYActu = this.transform.position.y;
@@ -40,6 +41,8 @@ public class SpiderSimple : MonoBehaviour
         if (posYActu > posYMax && !posbool)
         {
             this.transform.position = new Vector2(this.transform.position.x, posYActu - dt);
+            web.transform.localScale = new Vector2(web.transform.localScale.x, web.transform.localScale.y + (speed * 0.05f) / 0.5f  );
+            web.transform.position = new Vector2(web.transform.position.x , web.transform.position.y + (speed * 0.025f) / 0.5f );
             timeActif += dt;
         }
         else
@@ -51,12 +54,14 @@ public class SpiderSimple : MonoBehaviour
         if (posbool && posYActu < posInit.y)
         {
             this.transform.position = new Vector2(this.transform.position.x, posYActu + dt);
+            web.transform.localScale = new Vector2(web.transform.localScale.x, web.transform.localScale.y - (speed * 0.05f) / 0.5f);
+            web.transform.position = new Vector2(web.transform.position.x, web.transform.position.y - (speed * 0.025f) / 0.5f);
             timeActif += dt;
         }
         else
         {
             posbool = false;
-            }
+        }
         
     }
 
