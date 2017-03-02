ef72e4a9c5de805e91c38d3fc8545c5331cc996b
Nikosaul <Nicolas.saul@etu.univ-lyon1.fr>
2017-03-02 22:52:55 +0100

    Sol Qui tomeb quand on marche dessus au bout d'un moment

---
+++ Gold&Retriever/Assets/Script/Chunk + Sol/SolQuiTombe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolQuiTombe : MonoBehaviour {
+
+    [SerializeField]
+    private LayerMask player;
+
+    [SerializeField]
+    private GameObject box;
+
+    [SerializeField]
+    private float pointDeVie = 100; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Physics2D.OverlapBox(new Vector2(box.transform.position.x, box.transform.position.y), new Vector2(1, 0.2330246f), 0, player))
+        {
+            pointDeVie--; 
+            if(pointDeVie < 0)
+            {
+                Destroy(this.gameObject); 
+            }
+        }
+    }
+}
