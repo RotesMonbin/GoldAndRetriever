0612cb45ef009b198f040b7449a1e11151dca34c
Nikosaul <Nicolas.saul@etu.univ-lyon1.fr>
2017-03-26 20:13:53 +0200

    Noveau chunk + nouveau subchunk + fix bug + nouvelle palteforme

---
+++ Gold&Retriever/Assets/Script/Chunk + Sol/SolQuiApparait.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolQuiApparait : MonoBehaviour
+{
+
+    [SerializeField]
+    private LayerMask player;
+
+    [SerializeField]
+    private GameObject box;
+
+    [SerializeField]
+    private List <GameObject> listPlateform;
+
+    [SerializeField]
+    private float timeBtwSpawn = 1;
+
+    private bool actif = false; 
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Physics2D.OverlapBox(new Vector2(box.transform.position.x, box.transform.position.y), new Vector2(1, 0.2330246f), 0, player) && !actif)
+        {
+            actif = true; 
+            StartCoroutine(PlateformeGogo());
+        }
+    }
+
+
+    IEnumerator PlateformeGogo()
+    {
+        for(int i = 0; i < listPlateform.Count + 3; i++)
+        {
+            yield return new WaitForSeconds(timeBtwSpawn);
+            if (i < listPlateform.Count ) 
+                listPlateform[i].SetActive(true); 
+            if(i > 2)
+                listPlateform[i-3].SetActive(false);
+        }
+        actif = false; 
+
+
+    }
+
+}
--- Gold&Retriever/Assets/Script/Interactible/SubChunk.cs
+++ Gold&Retriever/Assets/Script/Interactible/SubChunk.cs
@@ -9,7 +9,7 @@ public class SubChunk : MonoBehaviour {
     // Use this for initialization
     void Awake () {
         int ind = Random.Range(0, SubChunks.Count);
-        GameObject instance= Instantiate(SubChunks[ind],this.transform.parent.transform);
+        GameObject instance= Instantiate(SubChunks[ind].gameObject,this.transform.parent.transform);
         instance.transform.position = this.gameObject.transform.position;
         Destroy(this.gameObject);
 	}
